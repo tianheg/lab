4ab615e9939d4027a3cb689e79a7a7c0e743f4ef
tianheg <yidajiabei@163.com>
2022-04-04 13:54:40 +0800

    feat: add my works

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,124 +1,93 @@
 <script setup>
 import SingleWork from './SingleWork.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+import dayjs from 'dayjs'
+
+import HtmlIcon from './icons/IconHtml.vue'
+import MapIcon from './icons/IconMap.vue'
+import RandomIcon from './icons/IconRandom.vue'
+import XiaomiIcon from './icons/IconXiaomi.vue'
+
+function formatDate(d) {
+  const date = dayjs(d)
+  // if (date.year() === dayjs().year())
+  //     return date.format('MMM D');
+  return date.format('MMM D, YYYY')
+}
+// formatDate() === now
 </script>
 
 <template>
   <SingleWork>
     <template #icon>
-      <DocumentationIcon />
+      <MapIcon />
     </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a target="_blank" href="https://vuejs.org/">official documentation</a>
-    provides you with all information you need to get started.
-  </SingleWork>
-  
-  <SingleWork>
-    <template #icon>
-      <DocumentationIcon />
+    <template #time>
+      {{ formatDate('2022-03-14') }} - {{ formatDate() }}
     </template>
-    <template #heading>Documentation</template>
+    <template #heading>杭州互联网公司地图</template>
 
-    Vue’s
-    <a target="_blank" href="https://vuejs.org/">official documentation</a>
-    provides you with all information you need to get started.
-  </SingleWork>
-  <SingleWork>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a target="_blank" href="https://vuejs.org/">official documentation</a>
-    provides you with all information you need to get started.
-  </SingleWork>
-  <SingleWork>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a target="_blank" href="https://vuejs.org/">official documentation</a>
-    provides you with all information you need to get started.
-  </SingleWork>
-
-  <SingleWork>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite</a>.
-    The recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>.
-    If you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank">Cypress</a> and
-    <a
-      href="https://docs.cypress.io/guides/component-testing/introduction"
-      target="_blank"
-      >Cypress Component Testing</a
-    >.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </SingleWork>
-
-  <SingleWork>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a target="_blank" href="https://pinia.vuejs.org/">Pinia</a>,
-    <a target="_blank" href="https://router.vuejs.org/">Vue Router</a>,
-    <a target="_blank" href="https://test-utils.vuejs.org/">Vue Test Utils</a>,
-    and
-    <a target="_blank" href="https://github.com/vuejs/devtools">Vue Dev Tools</a
-    >. If you need more resources, we suggest paying
-    <a target="_blank" href="https://github.com/vuejs/awesome-vue"
-      >Awesome Vue</a
+    <a href="https://tianheg.github.io/hangzhou-it-map/">主页</a>，<a
+      href="https://github.com/tianheg/hangzhou-it-map"
+      >仓库</a
     >
-    a visit.
   </SingleWork>
 
   <SingleWork>
     <template #icon>
-      <CommunityIcon />
+      <RandomIcon />
     </template>
-    <template #heading>Community</template>
+    <template #time>
+      {{ formatDate('2021-12-14') }} - {{ formatDate('2022-03-04') }}
+    </template>
+    <template #heading>随机文章</template>
 
-    Got stuck? Ask your question on
-    <a target="_blank" href="https://chat.vuejs.org">Vue Land</a>, our official
-    Discord server, or
-    <a target="_blank" href="https://stackoverflow.com/questions/tagged/vue.js"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a target="_blank" href="https://news.vuejs.org">our mailing list</a> and
-    follow the official
-    <a target="_blank" href="https://twitter.com/vuejs">@vuejs</a>
-    twitter account for latest news in the Vue world.
+    <a href="https://tianheg.github.io/random-blog/">主页</a>，<a
+      href="https://github.com/tianheg/random-blog"
+      >仓库</a
+    >
   </SingleWork>
 
   <SingleWork>
     <template #icon>
-      <SupportIcon />
+      <XiaomiIcon />
     </template>
-    <template #heading>Support Vue</template>
+    <template #time>
+      {{ formatDate('2021-08-25') }} - {{ formatDate('2021-09-15') }}
+    </template>
+    <template #heading>生产实习-小米后台管理系统</template>
 
-    As an independent project, Vue relies on community backing for its
-    sustainability. You can help us by
-    <a target="_blank" href="https://vuejs.org/sponsor/">becoming a sponsor</a>.
+    <a href="https://www.yidajiabei.xyz/posts/xiaomi-system/">文章</a>，<a
+      href="https://github.com/tianheg/xiaomi2"
+      >仓库</a
+    >
+  </SingleWork>
+
+  <SingleWork>
+    <template #icon>
+      <HtmlIcon />
+    </template>
+    <template #time>
+      {{ formatDate('2021-05-08') }} - {{ formatDate() }}
+    </template>
+    <template #heading>50projects50days</template>
+
+    每天一个项目，用 HTML、CSS、JavaScript 完成，<a
+      href="https://tianheg.github.io/50projects50days/"
+      >主页</a
+    >，<a href="https://github.com/tianheg/50projects50days">仓库</a>
+  </SingleWork>
+
+  <SingleWork>
+    <template #icon>
+      <XiaomiIcon />
+    </template>
+    <template #time>
+      {{ formatDate('2021-05-08') }} - {{ formatDate('2021-05-12') }}
+    </template>
+    <template #heading>小米官网</template>
+
+    静态页面，<a href="https://tianheg.github.io/xiaomi/" target="_blank"
+      >主页</a
+    >，<a href="https://github.com/tianheg/xiaomi" target="_blank">仓库</a>
   </SingleWork>
 </template>
