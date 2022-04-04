ee97524d22a561f00cd26cd0d5a0553d27e31a49
tianheg <yidajiabei@163.com>
2022-04-04 13:53:21 +0800

    feat(style): remove nav, fix flex layout

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,6 @@ import Sidebar from '@/components/Sidebar.vue'
 
     <div class="wrapper">
       <Sidebar msg="Hello Guys! Welcome to my Lab" />
-
-      <nav>
-        <!-- <RouterLink to="/">Home</RouterLink> -->
-        <!-- <RouterLink to="/about">About</RouterLink> -->
-      </nav>
     </div>
   </header>
 
@@ -33,7 +28,6 @@ import Sidebar from '@/components/Sidebar.vue'
   max-width: 1280px;
   margin: 0 auto;
   padding: 2rem;
-
   font-weight: normal;
 }
 
@@ -60,66 +54,31 @@ a,
   }
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
 @media (min-width: 1024px) {
   body {
-    display: flex;
     place-items: center;
   }
 
   #app {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    display: flex;
+    flex-wrap: wrap;
+    position: relative;
     padding: 0 2rem;
   }
 
   header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    flex-grow: 10;
+    flex-basis: 300px;
+    align-self: flex-start;
+    position: sticky;
+    --offset: 2rem;
+    top: var(--offset);
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  main {
+    flex-basis: 0;
+    flex-grow: 999;
+    min-width: 40%;
   }
 }
 </style>
